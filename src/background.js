cf6b9ec2668fe5c3df6efba7ad8243ebb1fbb12b
Afternoon Pm <h2o.Drop2010@gmail.com>
2026-03-28 12:55:26 -0700

    style: Create .editorconfig, fix whitespace style

--- src/background.js
+++ src/background.js
@@ -6,16 +6,16 @@
 // See https://developer.chrome.com/extensions/background_pages
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+	if (request.type === 'GREETINGS') {
+		const message = `Hi ${
+			sender.tab ? 'Con' : 'Pop'
+		}, my name is Bac. I am from Background. It's great to hear from you.`;
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
+		// Log message coming from the `request` parameter
+		console.log(request.payload.message);
+		// Send a response message
+		sendResponse({
+			message,
+		});
+	}
 });
